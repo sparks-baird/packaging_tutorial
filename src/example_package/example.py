d1be2855bfdfb3521bcc0e23aea48cbc24c8a991
sgbaird <sterling.baird@icloud.com>
2021-10-14 23:46:26 -0600

    Update example.py

--- src/example_package/example.py
+++ src/example_package/example.py
@@ -1,2 +1,14 @@
 def add_one(number):
+    """Add one to number.
+
+    Parameters
+    ----------
+    number : float
+        Some number.
+
+    Returns
+    -------
+    float
+        number plus one.
+    """
     return number + 1
